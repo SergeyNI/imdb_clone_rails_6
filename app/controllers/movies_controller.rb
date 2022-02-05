1b19c2a973db7898bd771062b75b6ce61980d67a
Сергій Нумержицький <rist1cnum@gmail.com>
2022-02-05 17:33:39 +0200

    avg rate

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
   before_action :set_movie, only: %i[ show edit update destroy ]
   before_action :select_categories, only: %i[index new edit create]
-
+  # before_action :authenticate_user!
   # GET /movies or /movies.json
   def index
     @movies = Movie.all
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+1.upto 10 do |count|
+  user = User.create!(email: "user#{count}@example.com",password: "userpwd#{count}") 
+  puts(user.email)
+end
+Category.all.each { |cat| cat.destroy } 
 Category.create(name: "fantasy")
 Category.create(name: "blockbaster")
 Category.create(name: "romantic")
+
+
